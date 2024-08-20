3969742ec5295b4a488e3a629de3f7d3f04790b0
Maxim Vasilyev <mayxis@inbox.ru>
2024-08-20 23:22:24 +0300

    improve: medium/002

--- medium/002_Add-Two_Numbers.go
+++ medium/002_Add-Two_Numbers.go
@@ -11,46 +11,25 @@ type ListNode struct {
 // Time: 	O(n)
 // Space:	O(n)
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var result *ListNode = new(ListNode)
-	var l1next, l2next *ListNode = l1.Next, l2.Next
-	var next *ListNode = new(ListNode)
-	sum := l1.Val + l2.Val
-	value := sum % 10
-	remainder := sum / 10
-	result.Val = value
-	if l1next == nil && l2next == nil {
-		if remainder > 0 {
-			result.Next = next
-			next.Val = remainder
+	result := new(ListNode)
+	current := result
+	remainder := 0
+	for l1 != nil || l2 != nil || remainder != 0 {
+		sum := remainder
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
 		}
-		return result
-	}
-	result.Next = next
-	for l1next != nil || l2next != nil || remainder != 0 {
-		var l1value, l2value int
-		if l1next != nil {
-			l1value = l1next.Val
-			l1next = l1next.Next
-		} else {
-			l1value = 0
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
 		}
-		if l2next != nil {
-			l2value = l2next.Val
-			l2next = l2next.Next
-		} else {
-			l2value = 0
-		}
-		sum = l1value + l2value + remainder
-		value = sum % 10
 		remainder = sum / 10
-		next.Val = value
-		if l1next != nil || l2next != nil || remainder > 0 {
-			next.Next = new(ListNode)
-			next = next.Next
-		}
+		current.Next = new(ListNode)
+		current.Next.Val = sum % 10
+		current = current.Next
 	}
-
-	return result
+	return result.Next
 }
 
 func main() {

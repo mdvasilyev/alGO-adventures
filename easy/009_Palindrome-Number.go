ee9b2ad0f0aedda8a4ddc9a38bf6db4bc15e384b
Maxim Vasilyev <mayxis@inbox.ru>
2024-08-22 00:09:10 +0300

    feat: add another sln to easy/009

--- easy/009_Palindrome-Number.go
+++ easy/009_Palindrome-Number.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+// Time:	O(n)
+// Space:	O(n)
 func isPalindrome(x int) bool {
 	s := strconv.Itoa(x)
 	left := 0
@@ -17,6 +19,21 @@ func isPalindrome(x int) bool {
 		right--
 	}
 	return true
+}
+
+// Time:	O(n)
+// Space:	O(n)
+func isPalindrome2(x int) bool {
+	if x < 0 {
+		return false
+	}
+	original := x
+	reversed := 0
+	for x > 0 {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+	return original == reversed
 }
 
 func main() {

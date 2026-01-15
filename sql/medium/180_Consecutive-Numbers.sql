5803d2dda00089d58b1f20c0ed2b2bf41832d1c0
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-15 14:50:46 +0300

    feat: add another sln to sql/medium/180

--- sql/medium/180_Consecutive-Numbers.sql
+++ sql/medium/180_Consecutive-Numbers.sql
@@ -6,3 +6,7 @@ from (
     from logs
 ) t
 where t.num = t.next_num and t.next_num = t.next_next_num
+
+-- select distinct l1.num ConsecutiveNums
+-- from Logs l1 join Logs l2 on l1.id = l2.id + 1 join Logs l3 on l2.id = l3.id + 1
+-- where l1.num = l2.num and l2.num = l3.num

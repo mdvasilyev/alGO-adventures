a148b0dfa07b677cddf7903a5ac9be65024f93c2
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-17 16:12:50 +0300

    sln: easy/0169

---
+++ easy/0169_Majority-Element.go
@@ -0,0 +1,16 @@
+package main
+
+func majorityElement(nums []int) int {
+	res, cnt := 0, 0
+	for i := range nums {
+		if cnt == 0 {
+			res = nums[i]
+		}
+		if res == nums[i] {
+			cnt++
+		} else {
+			cnt--
+		}
+	}
+	return res
+}

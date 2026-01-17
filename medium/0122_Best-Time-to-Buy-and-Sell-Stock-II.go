16f071f4ae26cb74cb6bd5fcd31e78f1e0a8889e
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-17 18:01:52 +0300

    sln: medium/0122

---
+++ medium/0122_Best-Time-to-Buy-and-Sell-Stock-II.go
@@ -0,0 +1,11 @@
+package main
+
+func maxProfit(prices []int) int {
+	noShare := 0
+	withShare := -prices[0]
+	for i := 1; i < len(prices); i++ {
+		noShare = max(noShare, withShare+prices[i])
+		withShare = max(withShare, noShare-prices[i])
+	}
+	return noShare
+}

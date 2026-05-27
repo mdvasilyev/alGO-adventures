2f97840f1d24db1e820ed3a1879cdd2ce5042351
Maxim Vasilyev <mayxis@inbox.ru>
2026-05-27 17:23:16 +0300

    sln: algo/easy/0206

--- algo/easy/0206_Reverse-Linked-List.go
+++ algo/easy/0206_Reverse-Linked-List.go
@@ -29,6 +29,28 @@ func reverseList(head *ListNode) *ListNode {
 	return res.Next
 }
 
+// func reverseList(head *ListNode) *ListNode {
+//     var prev *ListNode
+//     cur := head
+//     for cur != nil {
+//         next := cur.Next
+//         cur.Next = prev
+//         prev = cur
+//         cur = next
+//     }
+//     return prev
+// }
+
+// func reverseList(head *ListNode) *ListNode {
+//     if head == nil || head.Next == nil {
+//         return head
+//     }
+//     newHead := reverseList(head.Next)
+//     head.Next.Next = head
+//     head.Next = nil
+//     return newHead
+// }
+
 func main() {
 	head1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: nil}}}}
 	head2 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: nil}}

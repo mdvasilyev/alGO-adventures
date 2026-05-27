74590dc5b4f030542e7c41a0d80f5818f300cadb
Maxim Vasilyev <mayxis@inbox.ru>
2026-05-27 15:25:25 +0300

    sln: algo/easy/0283

--- algo/easy/0283_Move-Zeroes.go
+++ algo/easy/0283_Move-Zeroes.go
@@ -1,33 +1,11 @@
 package main
 
-import "fmt"
-
 func moveZeroes(nums []int)  {
-    left, right := 0, len(nums)-1
-    for left < right {
-        for right >= 0 && nums[right] == 0 {
-            right--
-        }
-        if right >= 0 {
+    left := 0
+    for right := 0; right < len(nums); right++ {
+        if nums[right] != 0 {
             nums[left], nums[right] = nums[right], nums[left]
-        }
-        left++
-        right--
-    }
-    firstZeroPos := len(nums)
-    for i := range nums {
-        if nums[i] == 0 {
-            firstZeroPos = i
-            break
+            left++
         }
     }
-    for i, j := 0, firstZeroPos-1; i < j; i, j = i+1, j-1 {
-        nums[i], nums[j] = nums[j], nums[i]
-    }
-}
-
-func main() {
-	nums := []int{0,1,0,3,12}
-	moveZeroes(nums)
-	fmt.Println(nums)
 }

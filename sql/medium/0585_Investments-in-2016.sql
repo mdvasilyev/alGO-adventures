b19d2bb60cf03d8a08463e399869fe775d034aab
Maxim Vasilyev <mayxis@inbox.ru>
2026-06-03 21:33:03 +0300

    sln: sql/medium/0585

---
+++ sql/medium/0585_Investments-in-2016.sql
@@ -0,0 +1,12 @@
+select round(sum(tiv_2016)::numeric, 2) tiv_2016
+from Insurance
+where tiv_2015 in
+(select tiv_2015
+from Insurance
+group by tiv_2015
+having count(pid) > 1)
+and (lat, lon) in
+(select lat, lon
+from Insurance
+group by lat, lon
+having count(pid) = 1)
